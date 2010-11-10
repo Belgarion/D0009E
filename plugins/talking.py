99a0259d895d51d5cb02b0c506b224975ece9170
Magnus <yugge@epiktv.com>
2010-11-10 23:10:45 +0100

    small fix

--- plugins/talking.py
+++ plugins/talking.py
@@ -146,7 +146,7 @@ class Talking(PluginBase):
 
 	def delSentence(self, bot, channel, params):
 		try:
-			self.sentences.pop(params)
+			self.sentences.pop(int(params[0]))
 			bot.sendMessage("PRIVMSG", channel, "Sentence deleted.")
 		except:
 			bot.sendMessage("PRIVMSG", channel, "Failed to delete sentence.")
