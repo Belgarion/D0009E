bf58219a0de6acf6a7c95a49fd4cb00b11886c7f
Sebastian <sebbe1991+git@gmail.com>
2010-11-24 09:54:15 +0100

    Add !week

---
+++ plugins/week.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from pluginbase import PluginBase
+
+import datetime
+
+class Week(PluginBase):
+	def __init__(self, bot):
+		bot.registerCommand("!week", self.handleWeek)
+		bot.registerCommand("!vecka", self.handleWeek)
+		bot.addHelp("week", "Usage: !week")
+
+	def handleWeek(self, bot, channel, params):
+		bot.sendMessage("PRIVMSG", channel, "Week: %s" %
+				(datetime.datetime.now().strftime("%V")))
+
+mainclass = Week
