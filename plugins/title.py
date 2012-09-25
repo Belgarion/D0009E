b59833951a1aeb4b6b8f25809de30df7162cabb5
Yugge <yugge@ludd.ltu.se>
2012-09-25 00:26:05 -0700

    Added bit.ly to !title and fixed urllib (see desc)
    * Added bit.ly - add file api.key to base dir with following information
    : login=*username*&apiKey=*APIKEY*
    
    *fixed urllib2, needed another useragent for google.com and wikipedia.

--- plugins/title.py
+++ plugins/title.py
@@ -8,21 +8,66 @@ class Title(PluginBase):
 	def __init__(self, bot):
 		bot.registerCommand("!title", self.handleTitle)
 		bot.addHelp("title", "Usage: !title <url>")
+		bot.registerCommand("!link", self.handleTitle)
+		bot.addHelp("link", "Usage: !link <url>")
 
 	def handleTitle(self, bot, channel, params):
 		bot.sendMessage("PRIVMSG", channel, self.getTitle("%20".join(params)))
+	def shortenURL(self, url):
+		APIURL = "https://api-ssl.bitly.com"
+
+		#Get APIKEY
+		f = open("api.key","r")
+		apiKey = f.read()
+		f.close()
+
+		#Encode url
+		url = url.replace("!","%21")
+		url = url.replace("#","%23")
+		url = url.replace("$","%24")
+		url = url.replace("&","%26")
+		url = url.replace("'","%27")
+		url = url.replace("(","%28")
+		url = url.replace(")","%29")
+		url = url.replace("*","%2A")
+		url = url.replace("+","%2B")
+		url = url.replace(",","%2C")
+		url = url.replace("/","%2F")
+		url = url.replace(":","%3A")
+		url = url.replace(";","%3B")
+		url = url.replace("=","%3D")
+		url = url.replace("?","%3F")
+		url = url.replace("@","%40")
+		url = url.replace("[","%5B")
+		url = url.replace("]","%5D")
+
+		#Create String
+		GETURL = APIURL + "/v3/shorten?"
+		GETURL += apiKey + "&"
+		GETURL += "longURL=" + url
+		
+		try:
+			f = urllib2.urlopen(GETURL)
+			shortUrl = re.findall("\"url\": \"(.*?)\"",f.read())[0].replace("\/","/")
+			return shortUrl
+		except:
+			return ""
 
 	def getTitle(self, url):
 		try:
-			f = urllib2.urlopen(url)
+			shortUrl = self.shortenURL(url)
+
+			headers = { 'User-Agent' : 'Mozilla/5.0' }
+			req = urllib2.Request(url,None,headers)
+			f = urllib2.urlopen(req)
 			data = f.read()
 			f.close()
-		except urllib2.HTTPException, e:
-			return e
+		except urllib2.HTTPError, e:
+			return "Error: %s" % e
 
 		m = re.search("<title>\n?(.*)\n?</title>", data)
 		if m:
-			return m.group(1)
+			return "[ %s ] Title: %s" % (shortUrl,m.group(1))
 		return "Title not found"
 
 mainclass = Title
