cbc35d69413e9415ad6309f60f3f8defe468a79a
Albin Eldstål-Damlin <laeder.keps@gmail.com>
2012-10-25 08:27:50 +0200

    Added !bug command

--- plugins/others.py
+++ plugins/others.py
@@ -5,13 +5,18 @@ import time
 
 class Others(PluginBase):
 	def __init__(self, bot):
+		bot.registerCommand("!bug", self.handleBug)
 		bot.registerCommand("!echo", self.handleEcho)
 		bot.registerCommand("!ping", self.handlePing)
 		bot.registerCommand("!time", self.handleTime)
 		bot.registerCommand("!zzz", self.handleZZZ)
+		bot.addHelp("bug", "Usage: !bug")
 		bot.addHelp("echo", "Usage: !echo <arguments>")
 		bot.addHelp("time", "Prints current time")
 		bot.addHelp("!ping", "Pong")
+
+	def handleBug(self, bot, channel, params):
+		bot.sendMessage("PRIVMSG", channel, "https://github.com/Belgarion/D0009E/issues")
 
 	def handleEcho(self, bot, channel, params):
 		bot.sendMessage("PRIVMSG", channel, " ".join(params))
