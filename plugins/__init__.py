9a06e2a8a066a40d527e3b618ecc7e6a0f09d935
Johan Jatko <johan.jatko@outlook.com>
2015-10-26 18:28:07 +0100

    its only fair

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ['pluginbase', 'help', 'temp', 'title', 'calc', 'lunch', 'nextep',
 'dieroll', 'course', 'forecast', 'mkpass', 'others', 'quiet_messages', 'names',
 'aao', 'week', 'labbar', 'xmas', 'google', 'pick', 'tenta', 'twitter_reader',
-'talking', 'kolli', 'ticker', 'down', 'hyphenator']
+'talking', 'kolli', 'ticker', 'down', 'hyphenator', 'fairdieroll']
